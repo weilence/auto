75b6dc124ffb17763354c7352d23ccc65e2bef51
Weilence <weilence@163.com>
2022-11-02 00:07:25 +0800

    更新脚本

--- shell/vps.sh
+++ shell/vps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sh_ver="v0.0.1"
 
-github="https://raw.githubusercontent.com/Weilence/auto/main/shell"
+github="https://raw.githubusercontent.com/Weilence/auto/main"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
